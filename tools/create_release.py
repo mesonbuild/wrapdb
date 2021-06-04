77c8786b9789ea7f10bdadc785e0165577fd84bf
Xavier Claessens <xavier.claessens@collabora.com>
2021-06-04 15:12:11 -0400

    ci: Add workflow to sanity check PRs and create releases

---
+++ tools/create_release.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+
+# Copyright 2021 Xavier Claessens <xclaesse@gmail.com>
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+import configparser
+import shutil
+import hashlib
+import requests
+import tempfile
+import subprocess
+import json
+
+from pathlib import Path
+
+class CreateRelease:
+    def __init__(self, repo, token, tag):
+        print('Preparing release for:', tag)
+        self.tag = tag
+        self.name, self.version = self.tag.rsplit('_', 1)
+        self.repo = repo
+        self.token = token
+
+        with tempfile.TemporaryDirectory() as self.tempdir:
+            self.read_wrap()
+            self.find_upload_url()
+            self.create_patch_zip()
+            self.create_wrap_file()
+
+    def read_wrap(self):
+        filename = Path('subprojects', self.name + '.wrap')
+        self.wrap = configparser.ConfigParser(interpolation=None)
+        self.wrap.read(filename)
+        self.wrap_section = self.wrap[self.wrap.sections()[0]]
+
+    def create_patch_zip(self):
+        patch_directory = self.wrap_section.get('patch_directory')
+        if patch_directory is None:
+            return
+
+        directory = self.wrap_section.get('directory', self.name)
+        srcdir = Path('subprojects', 'packagefiles', patch_directory)
+        destdir = Path(self.tempdir, directory)
+        shutil.copytree(srcdir, destdir)
+        base_name = Path(self.tempdir, f'{self.tag}_patch')
+        shutil.make_archive(base_name.as_posix(), 'zip', root_dir=self.tempdir, base_dir=directory)
+
+        patch_filename = base_name.with_name(base_name.name + '.zip')
+        self.upload(patch_filename, 'application/zip')
+
+        h = hashlib.sha256()
+        h.update(patch_filename.read_bytes())
+        patch_hash = h.hexdigest()
+
+        del self.wrap_section['patch_directory']
+        self.wrap_section['patch_filename'] = patch_filename.name
+        self.wrap_section['patch_url'] = f'https://wrapdb.mesonbuild.com/v2/{self.tag}/get_patch'
+        self.wrap_section['patch_hash'] = patch_hash
+
+    def create_wrap_file(self):
+        filename = Path(self.tempdir, self.name + '.wrap')
+        with open(filename, 'w') as f:
+            self.wrap.write(f)
+
+        print('Generated wrap file:')
+        print(filename.read_text())
+        self.upload(filename, 'text/plain')
+
+    def find_upload_url(self):
+        api = f'https://api.github.com/repos/{self.repo}/releases'
+        headers = { 'Authorization': f'token {self.token}' }
+        response = requests.get(api, headers=headers)
+        response.raise_for_status()
+        for r in response.json():
+            if r['tag_name'] == self.tag:
+                self.upload_url = r['upload_url'].replace(u'{?name,label}','')
+                print('Found release:', self.upload_url)
+                return
+
+        content = {
+            'tag_name': self.tag,
+            'name': self.tag,
+        }
+        response = requests.post(api, headers=headers, json=content)
+        response.raise_for_status()
+        self.upload_url = response.json()['upload_url'].replace(u'{?name,label}','')
+        print('Created release:', self.upload_url)
+
+    def upload(self, path, mimetype):
+        headers = {
+            'Authorization': f'token {self.token}',
+            'Content-Type': mimetype,
+        }
+        params = { 'name': path.name }
+        response = requests.post(self.upload_url, headers=headers, params=params, data=path.read_bytes())
+        response.raise_for_status()
+
+def run(repo, token):
+    with open('releases.json', 'r') as f:
+        releases = json.load(f)
+    stdout = subprocess.check_output(['git', 'tag'])
+    tags = [t.strip() for t in stdout.decode().splitlines()]
+    for name, info in releases.items():
+        versions = info['versions']
+        latest_tag = f'{name}_{versions[0]}'
+        if latest_tag not in tags:
+            CreateRelease(repo, token, latest_tag)
+
+if __name__ == '__main__':
+    repo, token = sys.argv[1:]
+    run(repo, token)
---
+++ tools/sanity_checks.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python3
+
+# Copyright 2021 Xavier Claessens <xclaesse@gmail.com>
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+import json
+import subprocess
+import collections
+import configparser
+import re
+
+from pathlib import Path
+from utils import Version
+
+PERMITTED_FILES = ['meson.build', 'meson_options.txt', 'LICENSE.build']
+NO_TABS_FILES = ['meson.build', 'meson_options.txt']
+
+
+class TestReleases(unittest.TestCase):
+    def test_releases(self):
+        # Take list of git tags
+        stdout = subprocess.check_output(['git', 'tag'])
+        tags = [t.strip() for t in stdout.decode().splitlines()]
+
+        with open('releases.json', 'r') as f:
+            releases = json.load(f)
+
+        # All tags must be in the releases file
+        for t in tags:
+            name, version = t.rsplit('_', 1)
+            self.assertIn(name, releases)
+            self.assertIn(version, releases[name]['versions'])
+
+        # Verify keys are sorted
+        self.assertEqual(sorted(releases.keys()), list(releases.keys()))
+
+        # Get the list of wraps that has modified packagefiles
+        with open(Path.home() / 'files.json', 'r') as f:
+            changed_files = json.load(f)
+        self.changed_wraps = set()
+        for f in changed_files:
+            if f.startswith('subprojects/packagefiles'):
+                self.changed_wraps.add(f.split('/')[2])
+
+        for name, info in releases.items():
+            # Make sure we can load wrap file
+            config = configparser.ConfigParser()
+            config.read(f'subprojects/{name}.wrap')
+            self.assertEqual(config.sections()[0], 'wrap-file')
+            wrap_section = config['wrap-file']
+            self.check_has_no_path_separators(wrap_section['directory'])
+            self.check_has_no_path_separators(wrap_section['source_filename'])
+
+            # Basic checks
+            self.assertTrue(re.fullmatch('[a-z][a-z0-9._-]*', name))
+            patch_directory = wrap_section.get('patch_directory')
+            if patch_directory:
+                patch_path = Path('subprojects', 'packagefiles', patch_directory)
+                self.assertTrue(patch_path.is_dir())
+                # FIXME: Not all wraps currently complies, only check for wraps we modify.
+                if name in self.changed_wraps:
+                    self.assertTrue(Path(patch_path, 'LICENSE.build').is_file())
+                    self.check_files(patch_path)
+
+            # Make sure it has the same deps/progs provided
+            progs = []
+            deps = []
+            if 'provide' in config.sections():
+                provide = config['provide']
+                progs = [i.strip() for i in provide.get('program_names', '').split(',')]
+                deps = [i.strip() for i in provide.get('dependency_names', '').split(',')]
+                for k in provide:
+                    if k not in {'dependency_names', 'program_names'}:
+                        deps.append(k.strip())
+            progs = [i for i in progs if i]
+            deps = [i for i in deps if i]
+            self.assertEqual(sorted(progs), sorted(info['program_names']))
+            self.assertEqual(sorted(deps), sorted(info['dependency_names']))
+
+            # Verify versions are sorted
+            versions = info['versions']
+            self.assertGreater(len(versions), 0)
+            versions_obj = [Version(v) for v in versions]
+            self.assertEqual(sorted(versions_obj, reverse=True), versions_obj)
+
+            # The first version could be a new release, all others must have
+            # a corresponding tag already.
+            for i, v in enumerate(versions):
+                t = f'{name}_{v}'
+                ver, rev = v.rsplit('-', 1)
+                self.assertTrue(re.fullmatch('[a-z0-9._]+', ver))
+                self.assertTrue(re.fullmatch('[0-9]+', rev))
+                if i == 0:
+                    self.check_source_url(name, wrap_section, ver)
+                if i == 0 and t not in tags:
+                    self.check_new_release(name, info, wrap_section)
+                else:
+                    self.assertIn(t, tags)
+
+    def check_has_no_path_separators(self, value):
+        self.assertNotIn('/', value)
+        self.assertNotIn('\\', value)
+
+    def check_source_url(self, name, wrap_section, version):
+        if name == 'sqlite3':
+            segs = version.split('.')
+            assert(len(segs) == 3)
+            version = segs[0] + segs[1] + '0' + segs[2]
+        elif name == 're2':
+            version = f'{version[:4]}-{version[4:6]}-{version[6:8]}'
+        source_url = wrap_section['source_url']
+        version_ = version.replace('.', '_')
+        self.assertTrue(version in source_url or version_ in source_url,
+                        f'Version {version} not found in {source_url}')
+
+    def check_new_release(self, name, info, wrap_section):
+        if not info.get('skip_ci', False):
+            options = []
+            for o in info.get('build_options', []):
+                if ':' not in o:
+                    options.append(f'-D{name}:{o}')
+                else:
+                    options.append(f'-D{o}')
+            subprocess.check_call(['meson', 'setup', '_build', f'-Dwraps={name}'] + options)
+            subprocess.check_call(['meson', 'compile', '-C', '_build'])
+            subprocess.check_call(['meson', 'test', '-C', '_build'])
+        else:
+            subprocess.check_call(['meson', 'subprojects', 'download', name])
+
+    def is_permitted_file(self, filename):
+        if filename in PERMITTED_FILES:
+            return True
+        if filename.endswith('.h.meson'):
+            return True
+        return False
+
+    def check_files(self, patch_path):
+        tabs = []
+        not_permitted = []
+        for f in patch_path.rglob('*'):
+            if f.is_dir():
+                continue
+            elif not self.is_permitted_file(f.name):
+                not_permitted.append(f)
+            elif f.name in NO_TABS_FILES and '\t' in f.read_text():
+                tabs.append(f)
+        if tabs:
+            tabs_str = ', '.join([str(f) for f in tabs])
+            self.fail('Tabs in meson files are not allows: ' + tabs_str)
+        if not_permitted:
+            not_permitted_str = ', '.join([str(f) for f in not_permitted])
+            self.fail('Not permitted files found: ' + not_permitted_str)
+
+
+if __name__ == '__main__':
+    unittest.main()
