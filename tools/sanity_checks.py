da58c74369a8dbc282e9900f7bfe86c7b5d15c8b
Eli Schwartz <eschwartz@archlinux.org>
2022-01-17 14:21:41 -0500

    ci_config: rename skip_* to broken_* and comment about its purpose
    "Documentation is not there to be read" and all that. People keep on
    trying to actually use this, ignoring README.md, and the original
    comment was not helpful regarding its purpose.

--- tools/sanity_checks.py
+++ tools/sanity_checks.py
@@ -59,7 +59,7 @@ class TestReleases(unittest.TestCase):
             cls.ci_config = json.load(f)
 
         system = platform.system().lower()
-        cls.skip = cls.ci_config[f'skip_{system}']
+        cls.skip = cls.ci_config[f'broken_{system}']
         cls.fatal_warnings = os.environ.get('TEST_FATAL_WARNINGS', 'yes') == 'yes'
 
     def test_releases_json(self):
@@ -171,7 +171,7 @@ class TestReleases(unittest.TestCase):
                     if i == 0 and t not in self.tags:
                         with self.subTest(step='check_new_release'):
                             self.check_new_release(name)
-                            with self.subTest(f'If this works now, please remove it from skip_{platform.system().lower()}!'):
+                            with self.subTest(f'If this works now, please remove it from broken_{platform.system().lower()}!'):
                                 self.assertNotIn(name, self.skip)
                             self.check_meson_version(name, ver, patch_path)
                     else:
