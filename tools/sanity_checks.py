17e71308b91cb1762491e671b62801628cbcc88d
Rosen Penev <rosenp@gmail.com>
2022-12-20 19:09:32 -0800

    box2d: replace outdated doctest.h with dependency
    The latter is newer. It's what should be used anyway.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- tools/sanity_checks.py
+++ tools/sanity_checks.py
@@ -29,6 +29,9 @@ from utils import Version, is_ci, is_debianlike, is_linux, is_macos, is_windows
 
 PERMITTED_FILES = ['generator.sh', 'meson.build', 'meson_options.txt', 'LICENSE.build']
 PER_PROJECT_PERMITTED_FILES = {
+    'box2d': [
+        'doctest.h'
+    ],
     'jbigkit': [
         'jbig.def',
         'jbig85.def'
