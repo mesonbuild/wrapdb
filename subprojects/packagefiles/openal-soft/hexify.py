7b3eb116ee78fda96a7f69cbff137f674de710e4
Rosen Penev <rosenp@gmail.com>
2023-04-04 21:38:14 -0700

    openal-soft: update to 1.23.0
    Ran hexify.py through pylint.
    
    Various fixes for Windows.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- subprojects/packagefiles/openal-soft/hexify.py
+++ subprojects/packagefiles/openal-soft/hexify.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 
-import os, sys
+"""
+This import is to open the files.
+"""
+import sys
 
-ifile = sys.argv[1]
-ofile = sys.argv[2]
-varname = sys.argv[3]
-
-bs = open(ifile, 'rb').read()
-with open(ofile, 'w') as of:
-    of.write(f'const unsigned char {varname}[] = {{\n')
-    for b in bs:
-        hex_value = hex(b)
-        of.write(f'    {hex_value},\n')
-    of.write('};')
+with open(sys.argv[1], 'rb') as bs:
+    with open(sys.argv[2], 'w', encoding='utf-8') as of:
+        for b in bs.read():
+            hex_value = hex(b)
+            of.write(f'    {hex_value},\n')
