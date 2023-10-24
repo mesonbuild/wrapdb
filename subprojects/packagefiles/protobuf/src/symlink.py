1471dd83e29df8c19f751d041ac16336f6e11696
Mykola Dolhyi <0xb000@gmail.com>
2023-10-24 18:26:13 +0300

    protobuf: fix coding style for symlink.py

--- subprojects/packagefiles/protobuf/src/symlink.py
+++ subprojects/packagefiles/protobuf/src/symlink.py
@@ -1,33 +1,40 @@
 #!/usr/bin/env python3
 
+"""
+Helper-script, a replacement to unix `ln -s [target] [alias]` command
+should work everywhere where meson does
+"""
+
+import os
+import sys
 from pathlib import Path
 
 
 def main(argv: [str]) -> int:
-	if len(sys.argv) != 3:
-		print(f"Usage: {argv[0]} [target] [alias]")
-		return -1
+    """Your script main entry point"""
 
-	target = Path(sys.argv[1])
-	alias = Path(sys.argv[2])
-	alias_dir = alias.absolute().parent
+    if len(sys.argv) != 3:
+        print(f"Usage: {argv[0]} [target] [alias]")
+        return -1
 
-	if not target.exists():
-		print("Target not found at:", target.absolute())
-		return -2
-	if not alias_dir.exists():
-		print("Alias directory not found at:", alias_dir)
-		return -3
+    target = Path(sys.argv[1])
+    alias = Path(sys.argv[2])
+    alias_dir = alias.absolute().parent
 
-	import os
-	rtarget = Path(os.path.relpath(target.absolute(), alias_dir))
-	print(f"creating symlink {alias} -> {rtarget}")
-	if alias.is_symlink():
-		alias.unlink()
-	alias.symlink_to(rtarget, target_is_directory=target.is_dir())
+    if not target.exists():
+        print("Target not found at:", target.absolute())
+        return -2
+    if not alias_dir.exists():
+        print("Alias directory not found at:", alias_dir)
+        return -3
 
-	return 0
+    rtarget = Path(os.path.relpath(target.absolute(), alias_dir))
+    print(f"creating symlink {alias} -> {rtarget}")
+    if alias.is_symlink():
+        alias.unlink()
+    alias.symlink_to(rtarget, target_is_directory=target.is_dir())
+
+    return 0
 
 if __name__ == "__main__":
-	import sys
-	exit(main(sys.argv))
+    sys.exit(main(sys.argv))
