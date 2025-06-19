dd5faed262d91de977fe0974db4202e0d727b926
Benjamin Gilbert <bgilbert@backtick.net>
2025-06-18 18:10:21 -0700

    ci: stop downloading source archives 15x per run
    Currently each CI job downloads all needed source archives directly from
    upstream.  There are currently 15 jobs per run, and many PRs require a
    non-trivial number of runs during development.  This wastes upstreams'
    bandwidth.
    
    Fix this in two parts:
    
    1. Add a prelude job that runs before the others.  Have it download the
    source archives of any modified wraps (sanity_checks.yml) or all wraps
    (build_all.yml).  This lets us verify that the upstream URLs are valid and
    match the expected SHA256 hashes.  Use a GitHub Actions cache to save
    these archives and reuse them in the other CI jobs.  Hash the source URLs,
    filenames, and SHA256 hashes into the cache key; subsequent runs can reuse
    the same cache if they haven't changed these parameters.
    
    2. Sanity checks also need to download sources for dependencies.  The
    dependency set varies across platforms and is unknown before `meson setup`
    runs, so we can't easily download it in advance.  However, we've already
    uploaded all of its sources to our own GitHub releases for use in
    source_fallback_url, so we don't need upstream servers at all.  In each CI
    job, rewrite all wraps for unmodified projects to point source_url at our
    own GitHub release.  Point source_fallback_url at the original source_url
    in case the GitHub release doesn't exist for some reason.
    
    In net, this reduces the upstream source downloads for each merged wrap
    update from at least 31 plus two per dependency per platform to exactly
    three: once in the PR, once in master (which uses a separate Actions
    cache), and once to create the GitHub release.  In the weekly rebuild, it
    reduces the downloads from eleven to one.

---
+++ tools/internalize_sources.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+
+# Copyright 2025 Benjamin Gilbert <bgilbert@backtick.net>
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+from argparse import ArgumentParser
+import configparser
+from hashlib import sha256
+from io import StringIO
+import json
+from pathlib import Path
+import subprocess
+
+def wrap_path(name: str) -> Path:
+    return Path('subprojects', f'{name}.wrap')
+
+
+def read_wrap(name: str) -> configparser.ConfigParser:
+    wrap = configparser.ConfigParser(interpolation=None)
+    wrap.read(wrap_path(name), encoding='utf-8')
+    return wrap
+
+
+class Internalizer:
+    def __init__(self, all=False):
+        with open('releases.json') as fh:
+            releases = json.load(fh)
+        tags = set(
+            subprocess.check_output(['git', 'tag'], text=True).splitlines()
+        )
+
+        self.download = []
+        self.rewrite = {}
+        for name, info in releases.items():
+            tag = f'{name}_{info["versions"][0]}'
+            if all or tag not in tags:
+                self.download.append(name)
+            else:
+                self.rewrite[name] = tag
+
+    def get_cache_key(self) -> str:
+        hash = sha256()
+        for name in self.download:
+            wrap = read_wrap(name)
+            wf = wrap['wrap-file']
+            parts = [
+                name,
+                wf['source_url'],
+                wf.get('source_fallback_url', ''),
+                wf['source_filename'],
+                wf['source_hash'],
+                '=====',
+            ]
+            for s in parts:
+                hash.update(s.encode() + b'\0')
+        return hash.hexdigest()[:16]
+
+    def download_sources(self) -> None:
+        if self.download:
+            subprocess.check_call(
+                ['meson', 'subprojects', 'download'] + self.download
+            )
+        else:
+            Path('subprojects', 'packagecache').mkdir(exist_ok=True)
+
+    def rewrite_wraps(self) -> None:
+        for name, tag in self.rewrite.items():
+            wrap = read_wrap(name)
+            wf = wrap['wrap-file']
+            wf['source_fallback_url'] = wf['source_url']
+            wf['source_url'] = f'https://github.com/mesonbuild/wrapdb/releases/download/{tag}/{wf["source_filename"]}'
+            buf = StringIO()
+            wrap.write(buf)
+            with wrap_path(name).open('w') as fh:
+                fh.write(buf.getvalue().strip() + '\n')
+        print(f'Rewrote source_url for {len(self.rewrite)} projects.')
+
+
+def main() -> None:
+    common = ArgumentParser(add_help=False)
+    common.add_argument(
+        '-a', '--all', action='store_true', help='select all projects'
+    )
+
+    parser = ArgumentParser(
+        prog='internalize_sources.py',
+        description='CI tool for avoiding unnecessary downloads of upstream source archives.',
+    )
+    subparsers = parser.add_subparsers(metavar='subcommand', required=True)
+
+    cache_key = subparsers.add_parser(
+        'cache-key', parents=[common], help='generate cache key'
+    )
+    cache_key.set_defaults(op='cache-key')
+
+    download = subparsers.add_parser(
+        'download', parents=[common], help='download sources for modified wraps'
+    )
+    download.set_defaults(op='download')
+
+    rewrite = subparsers.add_parser(
+        'rewrite', parents=[common],
+        help='redirect unmodified wraps to WrapDB GitHub releases'
+    )
+    rewrite.set_defaults(op='rewrite')
+
+    args = parser.parse_args()
+    intern = Internalizer(args.all)
+    if args.op == 'cache-key':
+        print(intern.get_cache_key())
+    elif args.op == 'download':
+        intern.download_sources()
+    elif args.op == 'rewrite':
+        intern.rewrite_wraps()
+
+
+if __name__ == '__main__':
+    main()
