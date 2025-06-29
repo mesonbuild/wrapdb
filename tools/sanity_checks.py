mesonbuild/wrapdb

commit b3925f58d54f856014ba5df5ce7970688fa9609b

sanity_checks: forbid default_options Meson < 1.8 ignores in subprojects
Meson < 1.8 ignores buildtype, debug, optimization, and a number of other
options when in subproject default_options.  Forbid these from
default_options since they create differential behavior across Meson
versions; also it's not clear that wraps should have opinions on their
values.

tools/sanity_checks.py
@@ -618,6 +618,22 @@ class TestReleases(unittest.TestCase):
                 except license_expression.ExpressionError as exc:
                     raise Exception('Invalid license; see https://spdx.org/licenses/') from exc
 
+        with self.subTest(step='check_default_options'):
+            for opt in project.get('default_options', []):
+                name, _ = opt.split('=', 1)
+                # Before Meson 1.8 these were ignored in subproject
+                # default_options, and in any case it's not clear that wraps
+                # should have an opinion about them.  We don't check for all
+                # non-per-subproject options, just the ones that seem likely
+                # to show up in a wrap.
+                if name in {'buildtype',
+                            'debug',
+                            'optimization',
+                            'prefer_static',
+                            'strip',
+                            'unity'}:
+                    raise Exception(f'{name} is not permitted in default_options')
+
     def check_files(self, subproject: str, patch_path: Path) -> None:
         tabs: list[Path] = []
         not_permitted: list[Path] = []

commit 7547c98c346935dcd20d11dfbd223ae19fab4fc9

sanity_checks: fix test failure if rewriter can't parse a default option
If the rewriter can't parse a default_options member, e.g. because of a
ternary operator, it claims that option is null.  Don't fail tests over
this.

Also adjust a type to anticipate null dict values once the rewriter
supports default_options dicts.

Fixes: b3925f58d5 ("sanity_checks: forbid default_options Meson < 1.8 ignores in subprojects")

tools/sanity_checks.py
@@ -646,7 +646,7 @@ class TestReleases(unittest.TestCase):
                             'unity'}:
                     raise Exception(f'{name} is not permitted in default_options')
 
-    def get_default_options(self, project: dict[str, T.Any]) -> dict[str, str]:
+    def get_default_options(self, project: dict[str, T.Any]) -> dict[str, str | None]:
         opts = project.get('default_options')
         if not opts:
             return {}
@@ -654,7 +654,7 @@ class TestReleases(unittest.TestCase):
             return opts
         elif isinstance(opts, str):
             opts = [opts]
-        return dict(opt.split('=', 1) for opt in opts)
+        return dict(opt.split('=', 1) for opt in opts if opt is not None)
 
     def check_files(self, subproject: str, patch_path: Path) -> None:
         tabs: list[Path] = []