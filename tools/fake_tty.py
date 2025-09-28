241267fac92ded562e92b24bb2d39992fa2ca921
Benjamin Gilbert <bgilbert@backtick.net>
2025-09-28 11:45:29 -0700

    ci: switch Ubuntu aarch64 job to Python 3.8
    Have one job that catches tool breakage on the minimum Python version.

--- tools/fake_tty.py
+++ tools/fake_tty.py
@@ -3,4 +3,9 @@
 import os, pty, sys
 
 os.environ['TERM'] = 'xterm-256color'
-sys.exit(os.waitstatus_to_exitcode(pty.spawn(sys.argv[1:])))
+status = pty.spawn(sys.argv[1:])
+if sys.version_info >= (3, 9):
+    sys.exit(os.waitstatus_to_exitcode(status))
+else:
+    # just check for success or failure
+    sys.exit(0 if os.WIFEXITED(status) and not os.WEXITSTATUS(status) else 1)
